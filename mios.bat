599976aaecc518cfe2fc00f799414ff6dd1d8f4a
Miguelzaj <109252843+Miguelzaj@users.noreply.github.com>
2022-07-13 20:38:55 -0300

    Add files via upload

---
+++ mios.bat
@@ -0,0 +1,24 @@
+:Mainmenu
+echo    Assistent MiOS
+echo =====================
+echo 1. Open Youtube
+echo 2. Open Facebook
+echo 3. Open Instagram
+echo 4. Open Twitter
+choice /c 1234
+if %errorlevel% == 1 goto Youtube
+if %errorlevel% == 2 goto Facebook
+if %errorlevel% == 3 goto Instagram
+if %errorlevel% == 4 goto Twitter
+:Youtube
+start www.youtube.com
+goto Mainmenu
+:Facebook
+start www.facebook.com
+goto Mainmenu
+:Instagram
+start www.instagram.com
+goto Mainmenu
+:Twitter
+start www.twitter.com
+goto Mainmenu
