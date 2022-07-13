081d44c47a07556b320039904e5c04dcf4f1e09e
Miguelzaj <109252843+Miguelzaj@users.noreply.github.com>
2022-07-13 20:45:30 -0300

    Update mios.bat

--- mios.bat
+++ mios.bat
@@ -5,12 +5,14 @@ echo 1. Open Youtube
 echo 2. Open Facebook
 echo 3. Open Instagram
 echo 4. Open Twitter
-echo 5. Mios Search
+echo 5. Mios Bot
 choice /c 12345
 if %errorlevel% == 1 goto Youtube
 if %errorlevel% == 2 goto Facebook
 if %errorlevel% == 3 goto Instagram
 if %errorlevel% == 4 goto Twitter
+if %errorlevel% == 5 goto Bot
+cls
 :Youtube
 start www.youtube.com
 goto Mainmenu
@@ -23,3 +25,19 @@ goto Mainmenu
 :Twitter
 start www.twitter.com
 goto Mainmenu
+:Bot
+echo hello , i am mios bot!
+echo 1. say hello
+echo 2. back
+choice /c 12
+if %errorlevel% == 1 goto Hello
+if %errorlevel% == 2 goto Mainmenu
+:Hello
+echo how are you?
+echo 1. say i am fine
+echo 2. back to main menu
+choice /c 12
+if %errorlevel% == 1 goto Fine
+:Fine
+echo What you want me to do for you
+gotol Mainmenu
